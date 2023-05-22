8335bddb030917b9225b201910ab78b657aae178
Dima9460 <129609066+Dima9460@users.noreply.github.com>
2023-05-22 08:38:01 -0500

    settings.py

--- project/src/cblog/settings.py
+++ project/src/cblog/settings.py
@@ -87,7 +87,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'database1', # database name in RDS is written here
         'USER': 'admin', # database master username in RDS is written here
-        'PASSWORD': 'Password12345',
+        'PASSWORD': 'Password1234',
         'HOST': 'blog-rds.chs2jc50cfzy.us-east-1.rds.amazonaws.com',  # database endpoint is written here
         'PORT': '3306' # database port is written here
     }
@@ -144,8 +144,8 @@ LOGIN_REDIRECT_URL = "blog:list"
 LOGIN_URL = "login"
 
 
-AWS_STORAGE_BUCKET_NAME = 'myblogbucket-eser' # please enter your s3 bucket name
-AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+AWS_STORAGE_BUCKET_NAME = 'dimabackupblog' # please enter your s3 bucket name
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % dimabackupblog
 AWS_S3_REGION_NAME = "us-east-1" # please enter your s3 region 
 AWS_DEFAULT_ACL = 'public-read'
 
